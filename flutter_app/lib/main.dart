324b1cf0bb9e5e8f7e918f194b25aeacd286201c
Tamir <1tamir198@gmail.com>
2020-03-03 15:44:44 +0200

    Blank appuploaded

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//MaterialApp root widget to enable me blank screen + working with material design things
+void main() => runApp(MaterialApp(
+  home: Text("Hey flutter"),
+
+
+));
+
