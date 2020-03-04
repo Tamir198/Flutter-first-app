f31b48a6bdb51b47ccd697070ec6f6ade8434060
Tamir <1tamir198@gmail.com>
2020-03-04 11:47:51 +0200

    Adding some basic UI and colors

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,8 +2,34 @@ import 'package:flutter/material.dart';
 
 //MaterialApp root widget to enable me blank screen + working with material design things
 void main() => runApp(MaterialApp(
-  home: Text("Hey flutter"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('First app'),
+      centerTitle: true,
+      backgroundColor: Colors.blue,
+
+    ),
+    body: Center(
+      //Center widget center everything that inside his
+      child: Text(
+        "Text in body",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.blue
 
 
+        )
+
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text("Click?"),
+      backgroundColor: Colors.red[700],
+    ),
+
+  )
 ));
 
