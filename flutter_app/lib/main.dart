ce70a8b7ade05341695cbc8d96838da8fad1b0f8
Tamir <1tamir198@gmail.com>
2020-03-08 19:18:57 +0200

    Adding 3 basic screens

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,35 +1,25 @@
 import 'package:flutter/material.dart';
+import 'pages/home_screen.dart';
+import 'pages/loading_screen.dart';
+import 'pages/choose_location_screen.dart';
 
 //MaterialApp root widget to enable me blank screen + working with material design things
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('First app'),
-      centerTitle: true,
-      backgroundColor: Colors.blue,
-
-    ),
-    body: Center(
-      //Center widget center everything that inside his
-      child: Text(
-        "Text in body",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.blue
-
-
-        )
-
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text("Click?"),
-      backgroundColor: Colors.red[700],
-    ),
-
-  )
+    home: Home(),
 ));
 
+class List extends StatefulWidget {
+  @override
+  _ListState createState() => _ListState();
+}
+
+class _ListState extends State<List> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
