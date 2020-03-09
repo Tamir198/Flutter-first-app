b074ca1ae02ac32e5faebc76dba5e01c26420d6d
Tamir <1tamir198@gmail.com>
2020-03-10 00:29:47 +0200

    Remove debug bunner

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,16 +4,18 @@ import 'pages/loading_screen.dart';
 import 'pages/choose_location_screen.dart';
 
 //MaterialApp root widget to enable me blank screen + working with material design things
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes:{
-      //in flutter this is hwo you navigate screens
-      '/': (context) => LoadingScreen(),
-      '/Home': (context) => Home(),
-      '/Location': (context) => ChooseLocation()
-    }
+void main() =>
+    runApp(MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          //in flutter this is hwo you navigate screens
+          '/': (context) => LoadingScreen(),
+          '/Home': (context) => Home(),
+          '/Location': (context) => ChooseLocation()
+        }
 
-));
+    ));
 
 class List extends StatefulWidget {
   @override
@@ -26,6 +28,7 @@ class _ListState extends State<List> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
 
     );
   }
